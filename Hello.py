87eed42969c140600255dbf66eb02a18e115e281
Troy Howatt <howatttr@StumpyHopAlong.local>
2020-03-01 08:44:26 -0400

    Committer: Troy Howatt <howatttr@StumpyHopAlong.local>

--- Hello.py
+++ Hello.py
@@ -1,5 +1,11 @@
 def widget():
   print('lay it on me ' + name +'!')
 
+def stomp():
+  print('The bear says stomp, stomp, stomp!')
+
+
 name=input('Who am I talking to?')
 widget()
+print(name, stomp())
+
